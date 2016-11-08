cd86027064ccfebfcdf16fee410669d7bd9cd281
Chris Csikszentmihalyi <robotic@gmail.com>
2016-11-08 13:17:59 +0000

    added python requirements

--- weather.py
+++ weather.py
@@ -1,25 +1,77 @@
 """!weather <place> returns a weather report from wttr for <place>"""
 from mattermost_bot.bot import listen_to
 from mattermost_bot.bot import respond_to
-
-try:
-    from urllib import quote
-except ImportError:
-    from urllib.request import quote
-
+import subprocess
+import logging
+import html2text
 import requests
 
+@respond_to('wet (.*)')
+def wet(message, searchterm):
+   r = requests.get('http://wttr.in/funchal')
+   h = html2text.HTML2Text()
+   t = str(h.handle(r.text))
+   t = t.split('┌─────────────┐')
+   t[3]=t[3].split('\n\n')[0]
+   keys = ('now', 'today', 'tomorrow', 'later')
+   weather = dict(zip(keys,t))
+   logging.info(weather.get(searchterm))
+   message.reply("""
+``
+{0}
+``
+""".format(weather.get(searchterm)))
 
-@respond_to('weather (.*)')
-def wiki(message, searchterm):
-    """return the top wiki search result for the term"""
-    if not searchterm:
-        searchterm = "funchal"
-    searchterm = quote(searchterm)
 
-    url = "http://wttr.in/"
-    url = url.format(searchterm)
-
-    result = requests.get(url)
-
-    message.reply(u"{0}".format(result).encode('ascii', 'ignore'))
+#@respond_to('weather (.*)')
+#def weather(message, searchterm):
+#    """return the weather search result for the term"""
+#    logging.info('before curl')
+#    cmd = [ 'curl','http://wttr.in/funchal']
+#    output = subprocess.Popen( cmd, stdout=subprocess.PIPE ).communicate()[0]
+#    t = output.decode()
+#    t = t.split('┌─────────────┐')
+#    t[3]=t[3].split('\n\n')[0]
+#    keys = ('now', 'today', 'tomorrow', 'later')
+#    weather = dict(zip(keys,t))
+#    logging.warning(weather.get(searchterm))
+#    message.reply("``\n{0}\n``".format(str(weather.get(searchterm))))
+#
+#test22 = """
+#``
+#    \  /       Partly cloudy
+#  _ /"".-.     21 °C          
+#    \_(   ).   ↓ 0 km/h       
+#    /(___(__)  10 km          
+#               0.2 mm   
+#``
+#"""
+#
+#@listen_to('wea_wea')
+#def wea_wea(message):
+#    message.comment(test22)
+#
+#@respond_to('ww')
+#def weather(message):
+#    import pycurl
+#    from io import BytesIO
+#    
+#    buffer = BytesIO()
+#    c = pycurl.Curl()
+#    c.setopt(c.URL, 'http://wttr.in/funchal')
+#    c.setopt(c.WRITEDATA, buffer)
+#    c.perform()
+#    c.close()
+#     
+#    body = buffer.getvalue()
+#    output = body.decode('iso-8859-1')
+#    #logging.warning(body)
+#    logging.warning(str(body))
+#    #logging.warning(output)
+#    logging.warning('About to try message.comment')
+#    # Body is a byte string.
+#    # We have to know the encoding in order to print it to a text file
+#    # such as standard output.
+#    message.comment(output)
+#    logging.warning('tried message.comment')
+#
